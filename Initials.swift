46cc580158a98876cc0e469e0386c58dfafe66bb
Blue689 <118864212+Blue689@users.noreply.github.com>
2022-11-23 12:12:25 -0700

    Add files via upload

---
+++ Initials.swift
@@ -0,0 +1,10 @@
+//Cam Tomlin
+//"I am an amateur RAW powerlifter! B:315 DL:405 S:495")
+
+print(" C C C   T T T T T")
+print("C     C      T")
+print("C            T")
+print("C            T")
+print("C            T")
+print("C     C      T")
+print(" C C C       T")
